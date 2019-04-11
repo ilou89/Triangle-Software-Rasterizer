14d6dda6ac8f7a7e9b87abf35580378be8083afb
kalexio <e-kalexio@hotmail.com>
2019-04-11 23:59:20 +0300

    modelLoader: Load all data from obj file into memory (untested)

--- modelLoader.cpp
+++ modelLoader.cpp
@@ -1,4 +1,5 @@
 #include "modelLoader.h"
+#include <cstdint>
 
 ModelLoader::ModelLoader()
 {
@@ -23,14 +24,56 @@ ModelLoader::loadFile(std::string filename)
     while(getline(myfile, line)) {
         std::istringstream stream(line);
         if(!line.compare(0, 2, "v ")) {
-            std::cout << line << std::endl;
             char trash;
             stream >> trash;
             float x, y, z;
             stream >> x;
             stream >> y;
             stream >> z;
-            Vec3f vec3(x, y, z);
+            Vec3f v(x, y, z);
+            vec3_v.push_back(v);
+        } else if(!line.compare(0, 2, "vt")) {
+            char trash;
+            stream >> trash;
+            float x, y, z;
+            stream >> x;
+            stream >> y;
+            stream >> z;
+            Vec3f vt(x, y, z);
+            vec3_vt.push_back(vt);
+        } else if(!line.compare(0, 2, "vn")) {
+            char trash;
+            stream >> trash;
+            float x, y, z;
+            stream >> x;
+            stream >> y;
+            stream >> z;
+            Vec3f vn(x, y, z);
+            vec3_vn.push_back(vn);
+        } else if(!line.compare(0, 2, "f ")) {
+            int vertexIndex[3];
+            int uvIndex[3];
+            int normalIndex[3];
+            char trash;
+            stream >> trash;
+
+            for(int i = 0; i < 3; ++i) {
+                stream >> vertexIndex[i];
+                stream >> trash;
+
+                stream >> uvIndex[i];
+                stream >> trash;
+
+                stream >> normalIndex[i];
+            }
+            Vec3i vertexIndexs(vertexIndex[0], vertexIndex[1], vertexIndex[2]);
+            vertexIndices.push_back(vertexIndexs);
+            Vec3i uvIndexs(uvIndex[0], uvIndex[1], uvIndex[2]);
+            uvIndices.push_back(uvIndexs);
+            Vec3i normalIndexs(normalIndex[0], normalIndex[1], normalIndex[2]);
+            normalIndices.push_back(normalIndexs);
+        } else {
+            std::cout << line << std::endl;
         }
     }
 
@@ -39,8 +82,3 @@ ModelLoader::loadFile(std::string filename)
     return true;
 }
 
-void
-ModelLoader::loadVertices(std::string& line)
-{
-
-}
--- modelLoader.h
+++ modelLoader.h
@@ -6,10 +6,18 @@
 #include <iostream>
 #include <sstream>
 #include "geometry.h"
+#include <vector>
 
 class ModelLoader {
 private:
     void loadVertices(std::string& line);
+    std::vector<Vec3f> vec3_v;
+    std::vector<Vec3f> vec3_vt;
+    std::vector<Vec3f> vec3_vn;
+
+    std::vector<Vec3i> vertexIndices;
+    std::vector<Vec3i> uvIndices;
+    std::vector<Vec3i> normalIndices;
 
 public:
     ModelLoader();
