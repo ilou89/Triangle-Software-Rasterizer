aef1455c98bed55c4019ca1bd71221c90a6623d2
kalexio <e-kalexio@hotmail.com>
2019-04-16 22:47:32 +0300

    custom and portable command line parsing

--- main.cpp
+++ main.cpp
@@ -10,38 +10,29 @@
 static const unsigned int WIDTH = 800;
 static const unsigned int HEIGHT = 600;
 
-static const char * const shortOptions = "f:hb";
-static const struct option longOptions[] = {
-    { "filename",  1, NULL, 'f' },
-    { "help",      0, NULL, 'h' },
-    { "bresenham", 1, NULL, 'b' },
-};
+static void printUsage(std::string name);
+static bool parseArguments(int argc, char *argv[], std::string *filename, int *choice);
 
-static const char * const usageTemplate =
-    "Usage: %s [ options ]\n"
-    " -f, --filename FILE    The path of the .obj file to read\n"
-    " -h, --help             Print this information\n"
-    " -b, --bresenham CHOICE Choose between multiple implementations of line drawing algorithm\n"
-    "                        Choices are: 1)simpleBresenham, 2)integerBresenham and 3)sliceBresenham\n";
+int main(int argc, char *argv[]) {
 
-static void printUsage();
-static void parseArguments(int argc, char * const argv[]);
+    std::string filename;
+    int choice;
 
-int main(int argc, char * const argv[]) {
-
-    parseArguments(argc, argv);
+    if (!parseArguments(argc, argv, &filename, &choice)) {
+        return 1;
+    }
 
     Bitmap bitmap(WIDTH, HEIGHT);
     IntegerBresenham simpleBresenham(bitmap);
 
     auto start = std::chrono::system_clock::now();
-    //for(uint32_t i = 0; i < 1000000; i++) {
+    for(uint32_t i = 0; i < 1000000; i++) {
         simpleBresenham.drawLines();
-    //}
+    }
 
     //load data
     ModelLoader mdloader;
-    if(!mdloader.loadFile("obj/cube.obj")) {
+    if(!mdloader.loadFile(filename)) {
         std::cerr << "unable to execute loadFile" << std::endl;
     }
 
@@ -52,32 +43,58 @@ int main(int argc, char * const argv[]) {
 
     bitmap.write("lines.bmp");
 
-
     return 0;
 }
 
-static void printUsage() {
-    std::cout << usageTemplate << std::endl;
+static void printUsage(std::string name) {
+    std::cerr << "Usage " << name << " [ options ]\n"
+              << "options:\n"
+              << "\t-h, --help\t\t Show this help message\n"
+              << "\t-f, --filename  FILE\t Specify the path of the .obj file\n"
+              << "\t-b, --bresenham CHOICE\t Specify the drawing algorithm\n"
+              << "\t\t\tChoices are: 1 for simpleBresenham\n"
+              << "\t\t\t             2 for integerBresenham\n"
+              << "\t\t\t             3 for sliceBresenham\n"
+              << std::endl;
 }
 
-static void parseArguments(int argc, char * const argv[]) {
-    int option;
+static bool parseArguments(int argc, char *argv[], std::string *filename, int *choice) {
+    if (argc == 1) {
+        printUsage(argv[0]);
+        std::cerr << "Running with default configuration\n"
+                  << "cube.obj file and simpleBresenham algorithm\n"
+                  << std::endl;
 
-    do {
-        option = getopt_long(argc, argv, shortOptions, longOptions, NULL);
-        switch (option) {
-            case 'h':
-                printUsage();
-                break;
-            case '?':
-                printUsage();
-                break;
-            case -1:
-                std::cout << "Run with the default line drawing algorithm (simpleBresenham)" << std::endl;
-                break;
-            default:
-                break;
+        *filename = "obj/cube.obj";
+        *choice = 1;
+
+        return true;
+    }
+
+    for (int i = 1; i < argc; ++i) {
+        std::string arg = argv[i];
+        if ((arg == "-h") || (arg == "--help")) {
+            printUsage(argv[0]);
+            return false;
+        } else if ((arg == "-f") || (arg == "-filename")) {
+            if (i + 1 < argc) {
+                *filename = argv[++i];
+            } else {
+                std::cerr << "--filename oprion requires one argument" << std::endl;
+                return false;
+            }
+        } else if ((arg == "-b") || (arg == "-bresenham")) {
+            if (i + 1 < argc) {
+                std::istringstream stream(argv[++i]);
+                stream >> *choice;
+                std::cout << *choice << std::endl;
+            } else {
+                std::cerr << "--bresenham oprion requires one argument" << std::endl;
+                return false;
+            }
         }
-    } while (option != -1);
+    }
+
+    return true;
 }
 
